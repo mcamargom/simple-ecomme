7d93c005288d71236460e3b404344cb8525db8af
mcamargom <82552559+mcamargom@users.noreply.github.com>
2021-06-27 18:41:16 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 	ini_set('display_errors',1);
 	error_reporting(-1);
-	define('DB_HOST', 'localhost');
-    define('DB_USER', 'root');
-    define('DB_PASSWORD', 'root');
+	define('DB_HOST', 'database-1.cnphqowdny18.us-east-1.rds.amazonaws.com');
+    define('DB_USER', 'admin');
+    define('DB_PASSWORD', 'admin1234');
     define('DB_DATABASE', 'idukan');
 ?>
